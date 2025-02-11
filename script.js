b93562f422118417d6dddb553684205e0c002ae4
AmalStudentWepro <163731290+AmalStudentWepro@users.noreply.github.com>
2025-02-11 18:21:55 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+// 1 
+for(i = 2; i <= 10; i += 2) {
+    console.log(i);
+}
+
+// 2
+let sum = 0;
+let e = 1;
+
+while (e <= 100) {
+    sum += e;
+    e++;
+}
+
+
+console.log(sum);
+
+// 3
+let count = 0;
+
+while (true) {
+    count++;
+    if (count === 7) {
+        console.log("Цикл остановлен");
+        break;
+    }
+}
+
+// это пишу я Амаль, я написал e во втором задании потому что e означает elem (элемент)
